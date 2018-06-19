62eeee65b600b26f2c8007408455820d896160d3
Alexandr Vlasov <alexander.vlasov@gmail.com>
2018-06-19 12:48:37 +0200

    Subtask 3 part 1: newlines support

--- calc.py
+++ calc.py
@@ -1,5 +1,7 @@
+import re
+
 def add(numbers):
     if numbers == "":
         return 0
-    lst = numbers.split(",")
+    lst = re.split(r"[,\n]", numbers)
     return sum(map(lambda x: int(x), lst))
--- calc_test.py
+++ calc_test.py
@@ -14,3 +14,13 @@ class CalcTest(unittest.TestCase):
 
     def test_ten(self):
         self.assertEqual(calc.add("4,5,6,7,8,9,0,11,915,45"), 1010)
+
+    def test_newlines(self):
+        self.assertEqual(calc.add("333\n666\n999"), 1998)
+
+    def test_mixed(self):
+        self.assertEqual(calc.add("9,10\n11\n12\n13,14"), 69)
+
+    def test_doubledelimiter(self):
+        with self.assertRaises(ValueError):
+            calc.add("1\n,2")
